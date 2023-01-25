47bdb76c37568231be82d8eddf49190af7ecf2ea
Sachin Rawat <sachinr@iitk.ac.in>
2023-01-25 15:27:01 +0530

    tested for 3D case

--- src/testgpe.py
+++ src/testgpe.py
@@ -7,8 +7,8 @@ import numpy as xp
 # import cupy as xp
 # from gpe.set_device import xp
 
-N = [256, 256, 1]
-L = [32, 32, 1]
+N = [256, 256, 256]
+L = [32, 32, 256]
 
 par = Params(N, L, g = 1, dt=0.0001, tmax = 0.001)
 G = gpe.GPE(par)
@@ -20,6 +20,7 @@ def wfc(x, y=0, z=0):
     return const * xp.exp(-(x**2 + 2*y**2 + 4*z**2)/(2*eps1)) + 2
     
 def pot(x, y=0, z=0):
+    return 0
     return 0.5*(x**2 + 4*y**2 + 16*z**2)
 
 # wfc= lambda x, y=0, z=0: xp.exp((-x**2 + y**2 + z**2)/2)+0.1
@@ -35,7 +36,7 @@ print(G.compute_energy())
 print(G.compute_kinetic_energy(), G.compute_quantum_energy() + G.compute_internal_energy())
 print(comp, incomp)    
 print('\n\n\n')
-G.evolve_ms()
+# G.evolve_ms()
 
 # G.evolve()
 # xp.random.seed(0)
